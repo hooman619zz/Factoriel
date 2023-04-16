6f4fc7bc9b4fdc1b3324012c22614175f33b8d76
hooman619zz <hoomankhanloo99@hotmail.com>
2023-04-16 17:03:30 +0330

    init

---
+++ Factoriel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factoriel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+// Hooman GhorbanKhanloo
+            int count;
+            count = Convert.ToInt32(Console.ReadLine());
+            BigInteger result = 1;
+
+            for (int i = 2; i <= count; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine($"{count}! = " + result);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
